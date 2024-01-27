5f98f9bae43ff909b68904cc77c5ae67e8884eb8
bobbycrooz <idrisloove@gmail.com>
2024-01-28 00:36:32 +0100

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
